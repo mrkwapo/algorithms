2857399763d7638dd9436db8ad19f366de7ddc80
mrkwapo <mrkwapo@gmail.com>
2019-09-08 16:54:06 -0400

    Update

---
+++ truncateAString.js
@@ -0,0 +1,18 @@
+function truncateString(str, num) {
+  //console.log(Number.isInteger(num))
+
+  if (str.length != num) {
+    var res = str.slice(0, num);
+    console.log(str.slice(0, num));
+
+    var answer = res + '...';
+    console.log(res + '...');
+    console.log(str.length);
+    return answer;
+  } else {
+    console.log(str);
+    return str;
+  }
+}
+
+truncateString('A-tisket a-tasket A green and yellow basket', 8);
