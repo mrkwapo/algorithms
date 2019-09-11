f07e08e71467410d1f9563ca463ecf6f02fba772
mrkwapo <mrkwapo@gmail.com>
2019-09-10 20:27:06 -0400

    completed

---
+++ timeIsUp2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int startingHour = get_int("Starting Hour: ");
+  int hoursPassed = get_int("Hours passed: ");
+
+  int days = hoursPassed / 24;
+
+  int timeNow = startingHour + hoursPassed % 12;
+
+  if (timeNow >= 13)
+  {
+    printf("The time is now %i:00\n", timeNow % 12);
+    printf(" %i Days have passed\n", days);
+  }
+  else
+  {
+    printf("The time is now %i:00\n", timeNow);
+    printf(" %i Days have passed\n", days);
+  }
+}
