d740f237f827e17ceed5652ae730390438eb9d82
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-09-12 16:20:10 -0400

    Create mutation3.js

---
+++ mutation3.js
@@ -0,0 +1,23 @@
+function mutation(arr) {
+var rawFirst= arr[0];
+var rawSecond= arr[1];
+
+var firstLowercased= rawFirst.toLowerCase();
+var secondLowercased= rawSecond.toLowerCase();
+
+var first= firstLowercased.split("");
+var second= secondLowercased.split("");
+
+console.log("This is the value in first arguement: " + first);
+console.log("This is the value in second arguement: " + second);
+
+for(var i = 0; i < first.length; i++){
+if(first.indexOf(second[i])<0){
+
+return false;
+}
+  return true;
+}
+}
+
+ mutation(["hello", "hey"]);
