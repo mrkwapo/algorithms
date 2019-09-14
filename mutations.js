d75ecb5cb99e968ce7cb4700937007606f5aa94f
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-09-14 12:40:43 -0400

    completed

--- mutations.js
+++ mutations.js
@@ -1,28 +1,10 @@
 function mutation(arr) {
-var rawFirst= arr[0];
-var rawSecond= arr[1];
-
-var first= rawFirst.toLowerCase();
-var second= rawSecond.toLowerCase();
-
-console.log("This is the value in first arguement: " + first);
-console.log("This is the value in second arguement: " + second);
-
-for(var i = 0; i <= first.length; i++ ){
-
-console.log(first[i])
-}
-
-for(var j = 0; j <= second.length; j++ ){
-if(!first.includes(second[j])){
-  console.log(second[j]);
-return false;
-
-}else{
-  console.log(second[j]);
-  return true; 
-}
-}
-}
-
-mutation(["hello", "hey"]);
+  var first = arr[0].toLowerCase();
+  var second = arr[1].toLowerCase();
+  
+  for (var i=0;i<second.length;i++) {
+    if (first.includes(second[i]) ==false)
+      return false;
+  }
+  return true;
+ }
