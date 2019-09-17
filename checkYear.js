d4ef8c70dddb5c7c1f6058e1a4228f156e6258e1
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-09-16 21:06:09 -0400

    Update checkYear.js

--- checkYear.js
+++ checkYear.js
@@ -1,24 +1,11 @@
-var givenMonth = 12;
-var givenYear = 1984;
-
+var readline = require('readline-sync');
+var givenMonth = readline.question("What month?");
+var givenYear = readline.question("What year?");;
 var d = new Date();
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
 var todayMonth = months[d.getMonth()];
 var currentYear= d.getFullYear();
-
 var currentMonthNumber = months.indexOf(todayMonth) + 1 ;
-
-console.log("This is the number of the current month: " + currentMonthNumber);
-
-console.log( " This is the number of the current year: " + currentYear);
-
-
-console.log( "This is the difference between the given month and the current month: " + (currentMonthNumber - givenMonth) );
-
 var monthDifference = currentMonthNumber - givenMonth;
-
-
-
 var yearDifference = currentYear - givenYear;
-console.log( "This is the difference between the given year and the current year: " + yearDifference + " years and " + Math.abs(monthDifference) + " months");
+console.log( "This is the difference between the given date and the current date: " + yearDifference + " years and " + Math.abs(monthDifference) + " months");
