cbb10701d1bbcfd043d441e5eb33cdece3fd8523
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-09-16 20:50:32 -0400

    Update checkYear.js

--- checkYear.js
+++ checkYear.js
@@ -1,26 +1,24 @@
 var givenMonth = 12;
-var givenDay = 30;
-
+var givenYear = 1984;
 
 var d = new Date();
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 var todayMonth = months[d.getMonth()];
-var d = new Date();
-var todayDay = d.getDate();
+var currentYear= d.getFullYear();
 
 var currentMonthNumber = months.indexOf(todayMonth) + 1 ;
 
 console.log("This is the number of the current month: " + currentMonthNumber);
 
-console.log( " This is the number of the current day: " + todayDay);
+console.log( " This is the number of the current year: " + currentYear);
 
 
-console.log( "This is the difference between the given month and today's month: " + (currentMonthNumber - givenMonth) );
-console.log( "This is the difference between the given month and today's month handling wrap around with module: " + (currentMonthNumber - givenMonth%12) );
+console.log( "This is the difference between the given month and the current month: " + (currentMonthNumber - givenMonth) );
+
+var monthDifference = currentMonthNumber - givenMonth;
 
 
-console.log( "This is the difference between the given day and today's day: " + (todayDay - givenDay) );
-console.log( "This is the difference between the given day and today's day handling wrap around with module: " + (todayDay - givenDay%30) );
 
-
+var yearDifference = currentYear - givenYear;
+console.log( "This is the difference between the given year and the current year: " + yearDifference + " years and " + Math.abs(monthDifference) + " months");
