35c64226975d7ada3b44f445761d25ea7275ae72
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-11-21 21:09:43 -0500

    Done!

--- missingLetter.js
+++ missingLetter.js
@@ -1,41 +1,33 @@
+//Intermediate Algorithm Scripting: Missing letters
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+
+
 function fearNotLetter(str) {
-  
+
   var alphabetStr = 'abcdefghijklmnopqrstuvwxyz';
   var notIncluded = [];
+
   //handles if no letters are missing
   if (alphabetStr.includes(str)) {
     console.log(undefined);
     return undefined;
   }
-//handles strings that start with a
+  //handles strings that start with a
   for (var i = 0; i < alphabetStr.length; i++) {
     if (str[i] !== alphabetStr[i] && alphabetStr[0] === 'a') {
-      // console.log(alphabetStr[i]);
       notIncluded.push(alphabetStr[i]);
     }
-
     //handle strings that don't start with a
-        if (str[i] !== alphabetStr[0]) {
-      //find str[i] in the alphabetStr and start looping through then push the letter in alphabetStr that doesn't match str[i]
-
-      for(var j = alphabetStr.indexOf(str[0]); j < alphabetStr.length; j++){
-        if(alphabetStr[j] !== str[i]){
-          //console.log(alphabetStr[j]);
+    if (str[0] !== alphabetStr[0]) {
+      for (var j = alphabetStr.indexOf(str[0]); j < alphabetStr.length; j++) {
+        if (!str.includes(alphabetStr[j])) {
           return alphabetStr[j];
         }
-     
-
       }
-      
     }
   }
-
-  //console.log(notIncluded[0]);
-  //return notIncluded[0]
+  return notIncluded[0]
 }
 
-//fearNotLetter("abce"); //should return "d".
- //fearNotLetter("abcdefghjklmno") //should return "i".
- fearNotLetter("stvwx") //should return "u".
-// fearNotLetter("bcdf") //should return "e".
- //fearNotLetter("abcdefghijklmnopqrstuvwxyz") //should return undefined.
+fearNotLetter("abce");
